660b61722582f6c8f5d32c5d5ac82c0baf4933a0
Darshan <2019HT66592@wilp.bits-pilani.ac.in>
2022-07-05 22:36:48 +0530

    adding cookie cutter scafolling to the project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='code to implement mlops using git dvc cml and aws',
+    author='Srija AS',
+    license='MIT',
+)
